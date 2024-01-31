378f17484a3c871c17e8d320148e7beaab2e6f6c
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-01-30 22:17:51 -0500

    Revert "clang format"
    This reverts commit 46ae7bfbed95929a20787fc297eba48123b12e27.

--- cpp11armadillotest/src/00_main.cpp
+++ cpp11armadillotest/src/00_main.cpp
@@ -4,11 +4,15 @@
 [[cpp11::register]] doubles eigen_sym_dbl(const doubles_matrix<>& x);
 [[cpp11::register]] list eigen_gen_mat(const doubles_matrix<>& x);
 [[cpp11::register]] list eigen_gen_dbl(const doubles_matrix<>& x);
-[[cpp11::register]] list eigen_gen_mat_complex_wrapper(const doubles_matrix<>& x);
-[[cpp11::register]] list eigen_gen_dbl_complex_wrapper(const doubles_matrix<>& x);
+[[cpp11::register]] list eigen_gen_mat_complex_wrapper(
+    const doubles_matrix<>& x);
+[[cpp11::register]] list eigen_gen_dbl_complex_wrapper(
+    const doubles_matrix<>& x);
 [[cpp11::register]] list eigen_gen_dbl_2(const doubles_matrix<>& x);
-[[cpp11::register]] list eigen_gen_mat_complex_wrapper_2(const doubles_matrix<>& x);
+[[cpp11::register]] list eigen_gen_mat_complex_wrapper_2(
+    const doubles_matrix<>& x);
 
 [[cpp11::register]] doubles_matrix<> ols_mat(const doubles_matrix<>& y,
                                              const doubles_matrix<>& x);
-[[cpp11::register]] doubles ols_dbl(const doubles_matrix<>& y, const doubles_matrix<>& x);
+[[cpp11::register]] doubles ols_dbl(const doubles_matrix<>& y,
+                                    const doubles_matrix<>& x);
