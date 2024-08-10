cc2bd36808bb2e78a53947330fb1542bd1df3b47
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-08-10 16:55:39 -0400

    armadillo cpp version

--- R/version.R
+++ R/version.R
@@ -1,3 +1,12 @@
+#' Get Armadillo version
+#' 
+#' Provides the Armadillo C++ library version name and number included in the
+#' package.
+#' 
+#' @export
+#' @return A string with the Armadillo version name and number
+#' @examples 
+#' armadillo_version()
 armadillo_version <- function() {
   finp <- system.file("include/armadillo/arma_version.hpp", package = "armadillo")
   lines <- readLines(finp)
