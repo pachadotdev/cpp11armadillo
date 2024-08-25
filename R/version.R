a248fd9e0c7cbdf2efc6a7c760245daefa98efb9
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-08-25 13:50:24 -0400

    don't include preprint in the built pkg

--- R/version.R
+++ R/version.R
@@ -8,20 +8,20 @@
 #' @examples 
 #' armadillo_version()
 armadillo_version <- function() {
-  finp <- system.file("include/armadillo/arma_version.hpp", package = "cpp11armadillo")
-  lines <- readLines(finp)
+  # finp <- system.file("include/armadillo/arma_version.hpp", package = "cpp11armadillo")
+  # lines <- readLines(finp)
  
-  number <- c(
-    grep("ARMA_VERSION_MAJOR", lines, value = T)[[1]],
-    grep("ARMA_VERSION_MINOR", lines, value = T)[[1]],
-    grep("ARMA_VERSION_PATCH", lines, value = T)[[1]]
-  )
-  number <- gsub("[^0-9]", "", number)
+  # number <- c(
+  #   grep("ARMA_VERSION_MAJOR", lines, value = T)[[1]],
+  #   grep("ARMA_VERSION_MINOR", lines, value = T)[[1]],
+  #   grep("ARMA_VERSION_PATCH", lines, value = T)[[1]]
+  # )
+  # number <- gsub("[^0-9]", "", number)
 
-  name <- grep("ARMA_VERSION_NAME", lines, value = T)[[1]]
-  name <- gsub(".*\"(.*)\".*", "\\1", name)
+  # name <- grep("ARMA_VERSION_NAME", lines, value = T)[[1]]
+  # name <- gsub(".*\"(.*)\".*", "\\1", name)
 
-  paste(name, paste(number, collapse = "."))
+  # paste(name, paste(number, collapse = "."))
 
-  # return("Stochastic Parrot 14.0.2")
+  return("Stochastic Parrot 14.0.2")
 }
