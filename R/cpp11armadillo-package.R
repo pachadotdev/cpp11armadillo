65eb9fb6593ec11558900d15dd530e4f78bc107b
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-12-17 13:00:46 -0500

    move cpp11 to imports

--- R/cpp11armadillo-package.R
+++ R/cpp11armadillo-package.R
@@ -1,2 +1,19 @@
 #' @keywords internal
 "_PACKAGE"
+
+#' Check that cpp11 can be used
+#' @keywords internal
+#' @return integer value
+check_cpp11 <- function() {
+  cpp11::cpp_source(
+    code = '
+    #include "cpp11/integers.hpp"
+
+    [[cpp11::register]] int add_one() {
+      int x = 0;
+      x++;
+      return x;
+    }
+    '
+  )
+}
